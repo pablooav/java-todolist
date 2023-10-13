3b9eb3cd2a777ea04b54fb00eee657d1bd2edbef
Pablo Valle <pablo.valle@crmservices.com.br>
2023-10-13 12:56:13 -0300

    First commit

---
+++ src/test/java/br/com/pablooav/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pablooav.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
